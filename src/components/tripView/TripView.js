0d0ac5391a8188c1dc391fdcef425470ff34b5d2
primogill <primoledeboergill@gmail.com>
2022-12-05 15:57:36 +0100

    removed broken map component from tripView

--- src/components/tripView/TripView.js
+++ src/components/tripView/TripView.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
 //components
-s
+
 //functions
 import { splitLink, findStop, dateToTime } from "../trainLookup/plannerFunctions";
 //other
